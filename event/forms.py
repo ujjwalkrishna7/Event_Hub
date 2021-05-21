a0cebe3aa91b526419a4922bbb7210843f975b64
Saurav S Purushothaman <saurav.kudajadri@gmail.com>
2021-05-21 17:32:17 +0530

    Calender added in Event_Form

--- event/forms.py
+++ event/forms.py
@@ -2,9 +2,11 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField,DateTimeField,DateField,TimeField,IntegerField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField,TimeField,IntegerField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, InputRequired
 from event.models import User # type: ignore
+from wtforms.fields.html5 import DateField
+
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -64,7 +66,7 @@ class EventForm(FlaskForm):
     name = StringField('Event Name', validators=[DataRequired()])
     description = TextAreaField('Description', validators=[DataRequired()])
     venue = StringField('Venue', validators=[DataRequired()])
-    date = DateField('Date', validators=[DataRequired()])
+    date = DateField('Date', format='%Y-%m-%d', validators=[DataRequired()])
     time = TimeField('Time',validators=[DataRequired()])
     max = IntegerField('Maximum Participants',validators=[DataRequired()])
     banner =FileField('Update Banner', validators=[FileAllowed(['jpg', 'png','jpeg','jfif']),InputRequired()])
