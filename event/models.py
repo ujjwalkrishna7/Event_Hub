ac2d3f5f7997c59c985f0eaffd4f516320d0fa64
rohith-crypto <rohitict1@gmail.com>
2021-05-24 12:35:05 +0530

    Removed some bugs during registration

--- event/models.py
+++ event/models.py
@@ -83,8 +83,8 @@ class Controller(ModelView):
 
 class Temp(db.Model,UserMixin):
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(50),unique=True,nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable = False) 
+    username = db.Column(db.String(50),nullable=False)
+    email = db.Column(db.String(120),nullable = False) 
     password = db.Column(db.String(60),nullable=False)
  
 
