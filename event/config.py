2266c53dafda959be935ca5be4f5aa42af85511c
Ujjwal Krishna <ujjwalkrishna7@gmail.com>
2021-05-25 03:37:31 +0530

    changes to database_url

--- event/config.py
+++ event/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
